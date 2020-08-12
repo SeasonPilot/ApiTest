a1cb8d968ccd970342fece416f0c4e148c6ec80e
SeasonPilot <pilot1@foxmail.com>
2020-08-12 23:57:34 +0800

    增加雪球股票列表获取

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -16,3 +16,23 @@ class TestRequests:
         r = requests.get('https://testerhome.com/api/v3/topics.json?limit=2')
         logging.info(r)
         logging.info(json.dumps(r.json(), indent=2))
+
+    def test_xueqiu_quote(self):
+        url = 'https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json'
+        # 这样写是错误的
+        # params = {
+        #     'cookie': {
+        #         'xq_a_token': '8940234022a656e1c577fe7c1e2791963527562b',
+        #         'u': '1844895900'
+        #     },
+        #     'User-Agent': 'Xueqiu Android 11.19',
+        #     'category': '1'
+        # }
+        r = requests.get(url,
+                         params={'category': '1'},
+                         cookies={'xq_a_token': '8940234022a656e1c577fe7c1e2791963527562b', 'u': '1844895900'},
+                         headers={'User-Agent': 'Xueqiu Android 11.19'}
+                         )
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(), indent=2))
+        assert r.json()['data']['category'] == 1
